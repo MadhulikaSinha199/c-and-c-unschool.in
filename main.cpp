106e19fbd1be8648d974b479cc31fff745b6d48b
MadhulikaSinha199 <69103383+MadhulikaSinha199@users.noreply.github.com>
2020-08-02 13:10:51 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,sum;
-    printf("enter three numbers");
-    scanf("%d%d%d",&a,&b,&c);
-    sum = a+b+c;
-    printf("sum : %d",sum);
+    int a,b,c;
+    printf("enter the two number");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("c:%d\n",c);
+    c=a-b;
+    printf("c:%d\n",c);
+    c=a*b;
+    printf("c:%d\n",c);
+    c=a/b;
+    printf("c:%d\n",c);
     return 0;
 }
+
+
